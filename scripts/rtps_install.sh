c5d4485a2046f08ee005adb23f56ecbd00094d86
Michael Starks <starksmichael224@gmail.com>
2021-03-22 23:00:22 -0400

    Removed sudo from cd command

--- scripts/rtps_install.sh
+++ scripts/rtps_install.sh
@@ -3,12 +3,12 @@
 sudo apt install -y cmake g++ python3-pip wget git libasio-dev libtinyxml2-dev libssl-dev openjdk-11-jre
 sudo pip3 install -U colcon-common-extensions vcstool
 sudo mkdir ~/Fast-DDS
-sudo cd ~/Fast-DDS
+cd ~/Fast-DDS
 sudo wget https://raw.githubusercontent.com/eProsima/Fast-DDS/master/fastrtps.repos
 sudo mkdir src
 sudo vcs import src < fastrtps.repos
 sudo colcon build
-sudo cd ~
+cd ~
 sudo git clone --recursive https://github.com/eProsima/Fast-DDS-Gen.git
-sudo cd ~/Fast-DDS-Gen
+cd ~/Fast-DDS-Gen
 sudo ./gradlew assemble
